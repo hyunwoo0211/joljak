a69da34e66e549f20b1768f04c72cde27a003b04
hyunwoo0211 <gusdn022111@gmail.com>
2018-08-28 17:03:18 +0900

    VO commit

---
+++ testproject/src/main/java/egovframework/example/sample/service/DDog.java
@@ -0,0 +1,58 @@
+package egovframework.example.sample.service;
+
+public class DDog {
+	
+	private String dog_id;
+	private String user_id;
+	private String dog_name;
+	private String dog_species;
+	private String dog_age;
+	private String dog_sex;
+	private String dog_content;
+	
+	
+	public String getDog_id() {
+		return dog_id;
+	}
+	public void setDog_id(String dog_id) {
+		this.dog_id = dog_id;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getDog_name() {
+		return dog_name;
+	}
+	public void setDog_name(String dog_name) {
+		this.dog_name = dog_name;
+	}
+	public String getDog_species() {
+		return dog_species;
+	}
+	public void setDog_species(String dog_species) {
+		this.dog_species = dog_species;
+	}
+	public String getDog_age() {
+		return dog_age;
+	}
+	public void setDog_age(String dog_age) {
+		this.dog_age = dog_age;
+	}
+	public String getDog_sex() {
+		return dog_sex;
+	}
+	public void setDog_sex(String dog_sex) {
+		this.dog_sex = dog_sex;
+	}
+	public String getDog_content() {
+		return dog_content;
+	}
+	public void setDog_content(String dog_content) {
+		this.dog_content = dog_content;
+	}
+	
+	
+}
